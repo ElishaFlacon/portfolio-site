bf8c7e7576f47ccd64204bdd5c5e263ea2df3d5f
Elisha Flacon <elishaflacon@gmail.com>
2023-03-29 00:36:08 +0500

    upd defaulImage and ListImage

--- src/components/ListImages.jsx
+++ src/components/ListImages.jsx
@@ -5,9 +5,15 @@ import '../styles/ListImages.css'
 
 function ListImages(props) {
 
-    const images = props.images.map(e =>
-        <DefaultImage image={e} key={e} imgSize={props.imgSize} />
-    );
+    const images = props.images.map((data) => {
+
+        if (!Array.isArray(data)) {
+
+            return <DefaultImage image={data} key={data} imageSize={props.imageSize} effect={props.effect} />
+        }
+
+        return <DefaultImage image={data[0]} key={data[0]} imageSize={props.imageSize} effect={props.effect} link={data[1]} />
+    });
 
 
     return (
--- src/components/UI/defaultImage/DefaultImage.jsx
+++ src/components/UI/defaultImage/DefaultImage.jsx
@@ -4,18 +4,51 @@ import classes from './DefaultImage.module.css';
 
 function DefaultImage(props) {
 
-    const imgSize = props.imgSize || 128;
+    const imgSize = props.imageSize || 128;
 
-    const sizeStyle = {
-        'max-width': `${imgSize}px`,
-        'max-height': `${imgSize}px`
+    const styles = {
+        'max-width': imgSize,
+        'max-height': imgSize,
     };
 
 
+    const checkEffect = (state) => {
+        switch (state) {
+            case 'rotate':
+                return classes.rotate;
+            case 'lens':
+                return classes.lens;
+            default:
+                break;
+        };
+    };
+
+    const imageClass = `
+        ${classes.image} 
+        ${checkEffect(props.effect)}
+    `;
+
+
+    const returnedObject = (link) => {
+        if (!link) {
+            return (
+                <div className={classes.container}>
+                    <img className={imageClass} src={require(`../../../images/${props.image}`)} style={styles} alt="" />
+                </div>
+            );
+        }
+
+        return (
+            <div className={classes.container}>
+                <a href={link}>
+                    <img className={imageClass} src={require(`../../../images/${props.image}`)} style={styles} alt="" />
+                </a>
+            </div>
+        );
+    }
+
     return (
-        <div className={classes.container}>
-            <img className={classes.image} src={require(`../../../images/${props.image}`)} style={sizeStyle} alt="" />
-        </div>
+        returnedObject(props.link)
     );
 }
 
