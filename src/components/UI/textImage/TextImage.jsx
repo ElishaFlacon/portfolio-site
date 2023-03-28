37c8bba211d9623c689f9bde4ba28f45fc7c87ea
Elisha Flacon <elishaflacon@gmail.com>
2023-03-29 00:38:17 +0500

    upd UI component textImage

--- src/components/UI/textImage/TextImage.jsx
+++ src/components/UI/textImage/TextImage.jsx
@@ -4,10 +4,22 @@ import classes from './TextImage.module.css';
 
 function TextImage(props) {
 
+    // первое значение в массиве это высота, второе это ширина картинки
+    const imageSize = props.imageSize || [256, 768];
+    const styles = {
+        'max-height': imageSize[0],
+        'max-width': imageSize[1],
+    };
+
 
     return (
         <div className={classes.container}>
-            <img className={classes.image} src={require(`../../../images/${props.image}`)} alt="" />
+            <img
+                className={classes.image}
+                src={require(`../../../images/${props.image}`)}
+                style={styles}
+                alt=''
+            />
         </div>
     );
 }
