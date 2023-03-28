7cfd7d82725b71f76b5d70bfb5524d2f76ba8155
Elisha Flacon <elishaflacon@gmail.com>
2023-03-29 00:38:39 +0500

    create projects and tools blocks

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import '../styles/Blocks.css'
+import ListImages from './ListImages';
+import TextImage from './UI/textImage/TextImage';
 
 
 function Projects(props) {
 
 
     return (
-        <div>Projects</div>
+        <div className='projects'>
+
+            <TextImage image={props.textImage} imageSize={props.textImageSize} />
+
+            <div className="projects__container">
+
+                <ListImages images={props.images} effect={props.effect} imageSize={props.defaultImageSize} />
+
+            </div>
+
+        </div>
     );
 }
 
--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+import '../styles/Blocks.css'
 import ListImages from './ListImages';
 import TextImage from './UI/textImage/TextImage';
 
 
 function Tools(props) {
 
+
     return (
         <div className='tools'>
 
-            <TextImage image={props.textImage} />
-            {/* <img src={require(`../images/${props.text}`)} alt="" /> */}
+            <TextImage image={props.textImage} imageSize={props.textImageSize} />
 
             <div className="tools__container">
 
-                <ListImages images={props.images} />
+                <ListImages images={props.images} effect={props.effect} imageSize={props.defaultImageSize} />
 
             </div>
 
