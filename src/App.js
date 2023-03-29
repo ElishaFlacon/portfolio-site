d210555d1827cfeb58a7056912300cf73598081c
Elisha Flacon <elishaflacon@gmail.com>
2023-03-30 01:46:51 +0500

    add find me block

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './styles/App.css';
 import About from './components/About';
 import Tools from './components/Tools';
 import Projects from './components/Projects';
+import FindMe from './components/FindMe';
 
 
 function App() {
 
     const aboutImages = ['hello.png', 'flacon.png'];
 
-    const toolsImages = ['js.png', 'python.png', 'temp.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png',];
-    const toolsText = 'temp_text.png';
+    const toolsImages = ['js.png', 'python.png', 'temp_image.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png', 'js.png', 'python.png',];
+    const toolsText = 'stack.png';
 
     const projectsImages = [['js.png', 'https://google.com'], ['js.png', 'https://google.com'], ['js.png', 'https://google.com'], ['js.png', 'https://google.com'], ['js.png', 'https://google.com'], ['js.png', 'https://google.com'],];
-
-    const projectsText = 'temp_text.png';
+    const projectsText = 'projects.png';
 
 
     return (
@@ -21,6 +21,7 @@ function App() {
             <About images={aboutImages} />
             <Tools images={toolsImages} textImage={toolsText} effect={'rotate'} />
             <Projects images={projectsImages} textImage={projectsText} effect={'lens'} defaultImageSize={200} />
+            <FindMe />
         </div>
     );
 }
