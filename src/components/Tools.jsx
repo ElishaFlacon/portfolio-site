76236a3dddde0b79a096bfcb856811d9e83bc5c1
Elisha Flacon <elishaflacon@gmail.com>
2023-03-30 22:38:23 +0500

    semicolon

--- src/components/Tools.jsx
+++ src/components/Tools.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../styles/Blocks.css'
+import '../styles/Blocks.css';
 import ListImages from './ListImages';
 import TextImage from './UI/textImage/TextImage';
 
