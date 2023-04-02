ad5cd482fb69e5bbfc8c6eaaeb22963ece525ac3
Elisha Flacon <elishaflacon@gmail.com>
2023-04-02 11:36:36 +0500

    Updates

---
+++ precache-manifest.825bffe7024b047588816e2bd6a1fd14.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "1be1034e7f912c2542c3",
+    "url": "/site-portfolio/static/css/main.a8ab6595.chunk.css"
+  },
+  {
+    "revision": "1be1034e7f912c2542c3",
+    "url": "/site-portfolio/static/js/main.1be1034e.chunk.js"
+  },
+  {
+    "revision": "1708fb4cc8667208f489",
+    "url": "/site-portfolio/static/js/1.1708fb4c.chunk.js"
+  },
+  {
+    "revision": "518e8a403ca9fed12e42",
+    "url": "/site-portfolio/static/js/runtime~main.518e8a40.js"
+  },
+  {
+    "revision": "7f465cab119466862ac39c105e1e9c23",
+    "url": "/site-portfolio/static/media/ai_face.7f465cab.png"
+  },
+  {
+    "revision": "4ea56c987a3354c049bce14efd85d976",
+    "url": "/site-portfolio/static/media/bulls.4ea56c98.png"
+  },
+  {
+    "revision": "c0b8b820936b88a8d75a0bd177b2b380",
+    "url": "/site-portfolio/static/media/calc.c0b8b820.png"
+  },
+  {
+    "revision": "89f20009cdb2accef50fc4032f957778",
+    "url": "/site-portfolio/static/media/calc_js.89f20009.png"
+  },
+  {
+    "revision": "c24fd0b5207eb487f092d03e00ef18d9",
+    "url": "/site-portfolio/static/media/coffee.c24fd0b5.png"
+  },
+  {
+    "revision": "71988ed90ebd310c6f63725099b66758",
+    "url": "/site-portfolio/static/media/compare.71988ed9.png"
+  },
+  {
+    "revision": "f42f83b7bf20313a3f8316a27ed5ba41",
+    "url": "/site-portfolio/static/media/flacon.f42f83b7.png"
+  },
+  {
+    "revision": "15178760b1be22a2fc10ebc08ceb291e",
+    "url": "/site-portfolio/static/media/solve_ege.15178760.png"
+  },
+  {
+    "revision": "b16d43a9ca643c06e437f559f2d29a11",
+    "url": "/site-portfolio/static/media/temp_image.b16d43a9.png"
+  },
+  {
+    "revision": "5cf37b8da58ea935b99cc7c5b660ebfb",
+    "url": "/site-portfolio/static/media/todo.5cf37b8d.png"
+  },
+  {
+    "revision": "9359fdcdc7920c1d49fe6568f92e3068",
+    "url": "/site-portfolio/index.html"
+  }
+];
